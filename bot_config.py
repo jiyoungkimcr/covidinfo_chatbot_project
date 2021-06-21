d4991180cbd818fb9f9854ec76d60e83c789a28f
Jiyoung Kim <53321802+jiyoungkimcr@users.noreply.github.com>
2021-06-21 21:07:03 +0200

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -3,13 +3,14 @@
 
 import telegram
 
-api_key = '1743843452:AAF8fd99PD6ZlWArtPMOSMyuCsiGKXI2gnc'
+api_key = 'here, input your chatbot api token key you got from botfather(telegram)'
 
 bot = telegram.Bot(token=api_key)
 
 # chat_id = bot.get_updates()[-1].message.chat_id   # <-this code is for checking chat_id of my bot
 # print(chat_id)
-chat_id = 1871793518 # this chat_id will be a bridge btw our code and telegram response, we will use this id and token key in the final code
+chat_id = # <- input your chat_id you got from 2 lines of code above in here  
+# this chat_id will be a bridge btw our code and telegram response, we will use this id and token key in the final code
 
 # In case we want to check if bot works well, we can try this simple welcoming sentence
 bot.sendMessage(chat_id = chat_id,
